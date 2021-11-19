a4263d1d93553882c68e8134db15205fe77d7c36
Anchit Lahkar <anchitlahkar0202@gmail.com>
2021-11-19 21:13:44 +0530

    19.11.2021

---
+++ JS/events.js
@@ -0,0 +1,54 @@
+AFRAME.registerComponent("cursor-event", {
+  schema: {
+    selectedId: { type: "string", default: "" },
+  },
+
+  init: function () {
+    // Do something when component first attached.
+    this.handelMouseEntry();
+    this.handelMouseLeave()
+  },
+
+  handelMouseEntry: function () {
+    this.el.addEventListener("mouseenter", () => {
+      const id = this.el.getAttribute("id");
+      const comicList = [
+        "eternals",
+        "im-captiain-america",
+        "iron-man-annual",
+        "the-death-of-doctor-strange",
+      ];
+
+      if (comicList.includes(id)) {
+        const placesContainer = document.querySelector("#comic-container");
+
+        placesContainer.setAttribute("cursor-event", {
+          selectedId: id,
+        });
+
+        this.el.setAttribute("material", {
+          color: "yellow",
+          opacity: 1,
+        });
+      }
+    });
+  },
+
+  handelMouseLeave: function () {
+    this.el.addEventListener("mouseleave", () => {
+      const {selectedId} = this.data
+
+      if(selectedId){
+        const el = document.querySelector(`#${selectedId}`)
+        const id = el.getAttribute("id")
+
+        if(id === selectedId){
+          el.setAttribute("material",{
+            color:"#fff",
+            opacity: 0.7
+          })
+        }
+      }
+    });
+  },
+});
